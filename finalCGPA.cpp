143492b89c3d31b1a6760f036329349121528a9d
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-11-05 09:51:43 +0530

    Changes In FinalCGPA file

--- finalCGPA.cpp
+++ finalCGPA.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -6,18 +6,19 @@ int main()
 {
     float cgpa, sem, goal, current_sgpa, final;
     cout << "Enter your CGPA: \n";
+
     cin >> cgpa;
     cout << "Enter your current semester (Whose result is not declared yet): \n";
     cin >> sem;
     cout << "Enter the CGPA you want to achieve by end of ENGG: \n";
     cin >> goal;
 
-    current_sgpa = cgpa * (sem-1);
-    final = (((goal * 8) - current_sgpa) / ((8-sem)+1));
+    current_sgpa = cgpa * (sem - 1);
+    final = (((goal * 8) - current_sgpa) / ((8 - sem) + 1));
 
-    if(final <= 10) 
+    if (final <= 10)
         cout << "You need to score " << final << " in all remaining semesters";
     else
-        cout <<"Sorry!! You can't achieve that much even if you score 10 sgpa in all remaining semesters";
+        cout << "Sorry!! You can't achieve that much even if you score 10 sgpa in all remaining semesters";
     return 0;
 }
