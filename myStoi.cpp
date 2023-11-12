246e81c890b88a6ffe8419bdf4131f24001ae0a0
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-11-12 20:39:14 +0530

    Updated myStoi Program

--- myStoi.cpp
+++ myStoi.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int num=0;
+int main()
+{
+    int num = 0;
     string s;
-    cout<< "Enter the number: "<<endl;
+    cout << "Enter the number: " << endl;
     cin >> s;
-    for(int i=0; s[i]!= '\0'; i++)
+    for (int i = 0; s[i] != '\0'; i++)
         num = (num * 10) + (s[i] - '0');
-    cout<< endl << "Number is "<<num<<endl;
+    cout << endl
+         << "Number is " << num << endl;
     return 0;
 }
